62416f337814da59f9715d2c1eacc11012089b26
RohMad <rohmad37@ymail.com>
2019-01-08 08:57:29 +0700

    Update IF-ELSE

---
+++ if-else.js
@@ -0,0 +1,15 @@
+function JalanSatu() {
+  var nilai = 1;
+  // if(nilai === 3) {
+  //   console.log("Benar")
+  // }else if (nilai ===2) {
+  //   console.log("Dua")
+  // }else{
+  //   console.log("Salah")
+  // }
+  if(nilai === 3) console.log("OK")
+  else console.log("Bukan");
+}
+
+
+JalanSatu()
