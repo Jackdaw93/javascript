cc7935e7b595d1d2644dcb30717d1cfe6e41ace1
RohMad <rohmad37@ymail.com>
2019-01-08 09:08:25 +0700

    Adding Switch Statement

---
+++ switch.js
@@ -0,0 +1,21 @@
+function testingSwitch() {
+  var nilai = "Dilo";
+  // var nilai = 10;
+
+  switch (nilai) {
+    // case 10:
+    case "Dilo" :
+      console.log("Sangat Bagus");
+      break;
+    case 8:
+      console.log("Baik");
+      break;
+    case 6:
+      console.log("Cukup");
+      break;
+    default:
+      console.log("Lainnya");
+  }
+}
+
+testingSwitch()
