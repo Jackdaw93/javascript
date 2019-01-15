3f12896f87e2190c18291550303a085c8b700526
RohMad <rohmad37@ymail.com>
2019-01-15 09:41:07 +0700

    Adding TipeData.js

---
+++ tipedata.js
@@ -0,0 +1,13 @@
+function tipeData() {
+  var data = '2';
+  console.log(typeof(data));
+
+  if (data === 2) {
+    return 'succes';
+  }else {
+    return 'gagal';
+  }
+
+}
+
+console.log(tipeData())
