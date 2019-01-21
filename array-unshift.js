3a193b11e35a731b63220bbe14bad4ecb111999e
RohMad <rohmad37@ymail.com>
2019-01-21 13:49:18 +0700

    Adding Array Unshift.js

---
+++ array-unshift.js
@@ -0,0 +1,9 @@
+function panggilUnshift() {
+  var data = ["Bandung","Jakarta","Medang","Malang"];
+  console.log(data);
+  data.unshift("Surabaya","Bali")
+
+  return data;
+}
+
+console.log(panggilUnshift());
