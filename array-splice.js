984b627b942e299dcce05ffb14f7aa60de56016e
RohMad <rohmad37@ymail.com>
2019-01-21 13:42:47 +0700

    Update Hapus Array with Splice

--- array-splice.js
+++ array-splice.js
@@ -1,8 +1,10 @@
 function panggilSplice() {
   var kota = ['Jakarta','Medan','Padang','Malang'];
   console.log(kota)
-  kota.splice(2,0,'Palembang') //Kita ingin menambahkan kota palembang diantara kota 'medan' dan 'padang'
-                              //array splice perbedaan apabila angka 0 diganti dengan angka 1 maka kota 'padang' akan hilah dan berganti kota 'palembang'
+  // kota.splice(2,0,'Palembang') //Kita ingin menambahkan kota palembang diantara kota 'medan' dan 'padang'
+                                  //array splice perbedaan apabila angka 0 diganti dengan angka 1 maka kota 'padang' akan hilah dan berganti kota 'palembang'
+
+    kota.splice(1,2) //Hapus dengan Splice: Index 1 adalah lokasi Index nya ex: 'Jakarta', Index 2 adalah jumlah yang akan kita hapus ex: 'Medan'&'Padang'
   return kota;
 }
 
