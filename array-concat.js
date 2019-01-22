5a3ba5a884f64cfc4d21e5f6d66ee1a5845db27a
RohMad <rohmad37@ymail.com>
2019-01-22 17:11:12 +0700

    Adding Array with Concat.js

---
+++ array-concat.js
@@ -0,0 +1,9 @@
+function panggilConcat() {
+  var kota = ["Jakarta","Medan","Bekasi","Malang"];
+  var kabupaten = ["Lampung Timur","Jombang","Kediri"];
+
+  var hasil = kota.concat(kabupaten)
+  return hasil;
+}
+
+console.log(panggilConcat());
