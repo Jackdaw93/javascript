698ce93f95cb59ff588db6f17253a68b8e9becc2
RohMad <rohmad37@ymail.com>
2019-01-23 14:05:41 +0700

    Adding Array with Join

---
+++ array-join.js
@@ -0,0 +1,8 @@
+function panggilJoin() {
+  var kota = ["Jakarta","Medan","Malang","Tangerang"];
+  console.log(kota);
+  var result = kota.join(",")
+  return result
+}
+
+console.log(panggilJoin());
