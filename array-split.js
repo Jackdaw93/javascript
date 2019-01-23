9d41a228026a370792f631185627a3f0cddaf598
RohMad <rohmad37@ymail.com>
2019-01-23 13:45:52 +0700

    Adding Array with Split

---
+++ array-split.js
@@ -0,0 +1,9 @@
+function panggilSplit() {
+  var kalimat = "Kita Sedang Belajar JS";
+  console.log(kalimat);
+
+  var result = kalimat.split(" ") //Spasi diantara kutip dua " " adalah variabel untuk menentukan string ke array
+  return result;
+}
+
+console.log(panggilSplit());
