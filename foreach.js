52572b8e844ac46b3d32657bf987b01b37015d24
RohMad <rohmad37@ymail.com>
2019-01-29 09:27:45 +0700

    Adding Array ForEach

---
+++ foreach.js
@@ -0,0 +1,11 @@
+function panggilforEach() {
+  var data = ['ayam','babi','chetah','domba']
+  data.forEach(function(/*item,*/ index, /*array*/){
+    //console.log(item);
+    console.log(index);
+    //console.log(array);
+
+  })
+}
+
+panggilforEach()
