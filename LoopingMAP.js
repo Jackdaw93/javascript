bab92e970f1572020c91a0bfe7afed3ea28f022a
RohMad <rohmad37@ymail.com>
2019-01-30 11:08:45 +0700

    Adding Looping MAP dan Perulangan

---
+++ LoopingMAP.js
@@ -0,0 +1,20 @@
+function panggilPerintahMAP() {
+
+//Looping Array MAP ver. 1
+/*  var dataKota = ['Jakarta','Balikpapan','Medan']
+  dataKota.map((item, index, array) => {
+    console.log(item);
+    console.log(index);
+    console.log(array);
+  });
+*/
+  var dataKota = ['Jakarta','Balikpapan','Medan'];
+  dataKota.map(function(/*item,*/ index, /*array*/){
+    //console.log(item);
+    console.log(index);
+    //console.log(array);
+  })
+
+}
+
+panggilPerintahMAP();
