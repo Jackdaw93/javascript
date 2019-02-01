905e113b37f4b65cc7b35e4735b55fe102e93ce4
RohMad <rohmad37@ymail.com>
2019-02-01 16:54:12 +0700

    Adding Parameter

---
+++ parameter.js
@@ -0,0 +1,14 @@
+// function panggilParameter(param1, param2, param3) {
+//   console.log(param1);
+//   console.log(param2);
+//   console.log(param3);
+// }
+//
+// panggilParameter('Jakarta','Medan','Balikpapan')
+
+function panggilParameter(idNegara, dataKota) {
+  console.log(idNegara);
+  console.log(dataKota);
+}
+
+panggilParameter(17081945, ['Jakarta','Medan','Balikpapan'])
