ed78b03dde13880d6d89f4ec6fd06a1f4946b86b
RohMad <rohmad37@ymail.com>
2019-02-06 09:53:16 +0700

    Update Balik Kata with looping FOR

--- balikKata.js
+++ balikKata.js
@@ -1,7 +1,19 @@
 function balikKata(kata) {
-  return kata.split('').reserve().join('');
+  var msg = '';
+  for (var i = kata.length-1; i >= 0; i--)
+  {
+    msg = msg + kata[i]
+  }
+
+  return msg;
+
+/* Balik Kata Dengan Split dll
+ return kata.split('').reverse().join('')
+*/
 
 }
 
 console.log(balikKata("Niomic!"));
 console.log(balikKata("JavaScript"));
+console.log(balikKata("alohahola"));
+console.log(balikKata("Jawa_Barat"));
