c2e48878c5bf3b591b2b3e09d5200afe256da47e
RohMad <rohmad37@ymail.com>
2019-02-11 10:50:55 +0700

    Adding Regular Expression with Flags

---
+++ RegExpFlags.js
@@ -0,0 +1,9 @@
+function panggilRegex(value) {
+  let str = "abcdefghijklmnopqrstuvwxyz"
+
+  console.log(str.search(/K/));
+  console.log(str.search(/K/i));
+  console.log(str.search(/k/i));
+}
+
+panggilRegex()
